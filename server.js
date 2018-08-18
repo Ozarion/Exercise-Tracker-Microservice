629cd9b95c1c2eb9e1ccb913d879dd497764c9cc
Ozarion <cozarion@gmail.com>
2018-08-18 21:22:00 +0500

    Completed the exercise tracker microservice

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
@@ -18,31 +18,103 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const validateUser = (text) => /^[a-zA-Z_][a-zA-Z0-9\s.\-_$@*!]*$/.test(text);
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+// Schema
+const userSchema = new mongoose.Schema({
+  username : {type : String, unique : true, index: true, required: true, lowercase: true, trim: true},
+  exercise : [{description : {type : String, trim : true, required: true},
+               duration : {type: Number, required: true}, 
+               date : {type : Date, default : new Date()}
+              }]
+});
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
+// User Model
+const User = mongoose.model('User', userSchema);
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+// Create New User
+app.post('/api/exercise/new-user', (req, res) => {
+  if (!validateUser(req.body.username)) {
+    return res.json({"ERROR" : "Invalid Username"});
   }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+  // Create the user and add to database
+  const newUser = new User({"username" : req.body.username});
+  newUser.save((err, doc) => {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(doc);
+  });
+});
+
+// Insert New Exercise data into given User
+app.post('/api/exercise/add', (req, res) => {
+  if (!validateUser(req.userId)) {
+    return res.json({"ERROR" : "Invalid Username"});
+  }
+  // If any of these null, return error
+  if (!req.body.description || !req.body.duration) {
+    return res.json({"ERROR" : "Must provide exercise duration and description"});
+  }
+  // Make exercise doc
+  const exercise = {
+        "description" : req.body.description,
+        "duration" : req.body.duration
+  };
+  // If date not null, add the date
+  if (req.body.date) {
+    exercise.date = req.body.date;
+  }
+  // Build lookup the query
+  const query = {"username" : req.body.userId.toLowerCase()};
+  // Build update operation for query
+  const queryOp = {
+    "$push" : {
+      "exercise" : exercise
+    }
+  };
+  // Fuckin Magic
+  User.findOneAndUpdate(query, queryOp).exec((err, doc) => {
+    if (err) {
+      return res.json(err);
+    };
+    res.json({"MESSAGE" : `${doc} updated`});
+  });
+});
+
+// Get user data from exercise database
+app.get('/api/exercise/log', (req, res) => {
+  if (!validateUser(req.query.userId)) {
+    return res.json({"ERROR" : "Invalid Username"});
+  }
+  const username = req.query.userId.toLowerCase();
+  const from = (req.query.from) ? new Date(req.query.from) : null;
+  const to = (req.query.to) ? new Date(req.query.to) : null;
+  const limit = Number(req.query.limit);
+  
+  User.findOne({"username" : username}).
+  select('-exercise._id -_id').
+  exec((err, doc) => {
+    if (err) {
+      return res.json(err);
+    }
+    const resp = {
+      "username" : doc.username,
+      "exercise" : doc.exercise
+    };
+    if (from) {
+      resp.exercise = resp.exercise.filter(e => e.date >= from);
+    }
+    if (to) {
+      resp.exercise = resp.exercise.filter(e => e.date <= to);
+    }
+    if (limit) {
+      resp.exercise = resp.exercise.slice(0, limit);
+    }
+    return res.json(resp);
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
